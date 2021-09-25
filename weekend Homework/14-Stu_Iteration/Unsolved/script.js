7478708867e74d7f7d7c919143165a643f5bd323
dennis-matthew-burgess <dennismatthewburgess@icloud.com>
2021-09-25 15:51:33 -0400

    14 done

--- weekend Homework/14-Stu_Iteration/Unsolved/script.js
+++ weekend Homework/14-Stu_Iteration/Unsolved/script.js
@@ -1,1 +1,8 @@
 //WRITE YOUR CODE BELOW
+
+var students = ["adam", "tony", "brian", "bryan", "steve", "aaron"]
+
+for(var i = 0; i < students.length; i++) {
+    console.log("Great to see you " + students[i] + "!")
+    
+}
