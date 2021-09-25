10bfee12ab9556c06c1cffede90b57e3aee81f14
dennis-matthew-burgess <dennismatthewburgess@icloud.com>
2021-09-25 15:08:30 -0400

    updating 12

--- weekend Homework/12-Stu_Arrays/Unsolved/script.js
+++ weekend Homework/12-Stu_Arrays/Unsolved/script.js
@@ -1,1 +1,14 @@
 // WRITE YOUR CODE HERE
+
+var studentArray = ["Alice", "Bob", "Charles", "Donny", "Edwin", "Frank"]
+
+console.log(studentArray.length);
+
+console.log("Welcome to the class " + studentArray[0]);
+console.log("Welcome to the class " + studentArray[1]);
+console.log("Welcome to the class " + studentArray[2]);
+console.log("Welcome to the class " + studentArray[3]);
+console.log("Welcome to the class " + studentArray[4]);
+
+studentArray.shift("Zed");
+console.log(studentArray);
