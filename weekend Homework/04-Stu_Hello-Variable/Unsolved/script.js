726264f938a7c04a53156b6598702b135c7f41d3
dennis-matthew-burgess <dennismatthewburgess@icloud.com>
2021-09-25 11:37:59 -0400

    compleated 6

--- weekend Homework/04-Stu_Hello-Variable/Unsolved/script.js
+++ weekend Homework/04-Stu_Hello-Variable/Unsolved/script.js
@@ -3,9 +3,9 @@ var personName = "Matthew";
 var pets = "1"
 var funFact = " I like to shoot guns"
 
-console.log("My name is " + personName)
-console.log("I have " + pets + " pet")
-console.log("fun fact:" + funFact)
+console.log("My name is " + personName + ".")
+console.log("I have " + pets + " pet" + ".")
+console.log("fun fact:" + funFact + ".")
 
 personName = "John"
 pets = "2"
