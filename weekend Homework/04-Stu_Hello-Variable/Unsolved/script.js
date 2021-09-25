a32d4c094af638e08cf3be20fba50af7400a9ce3
dennis-matthew-burgess <dennismatthewburgess@icloud.com>
2021-09-25 11:02:49 -0400

    first upload

---
+++ weekend Homework/04-Stu_Hello-Variable/Unsolved/script.js
@@ -0,0 +1,12 @@
+// Write Your JavaScript Code Here
+var personName = "Matthew"
+var pets = "1"
+var funFact = "i like to shoot guns"
+
+personName = "John"
+pets = "2"
+funFact = "I teach at too fast a pace."
+
+console.log("My name is " + personName);
+console.log("I have " + pets + " pets")
+console.log(funFact)
