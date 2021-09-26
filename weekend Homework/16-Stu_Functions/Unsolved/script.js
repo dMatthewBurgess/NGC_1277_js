0c660e7c5a596f6ce2ea439c7798a8e85dc8c6d6
dennis-matthew-burgess <dennismatthewburgess@icloud.com>
2021-09-26 09:06:02 -0400

    16 complete

--- weekend Homework/16-Stu_Functions/Unsolved/script.js
+++ weekend Homework/16-Stu_Functions/Unsolved/script.js
@@ -1,1 +1,29 @@
 // Write Your Code Below
+
+
+function equal(x, y) {
+    if (x === y) {
+        console.log("They are equal in type and value");
+    } else if (x == y) {
+        console.log("They are equal in value");
+    } else {
+        console.log("The values are not equal");
+    }
+    return;
+}
+
+
+
+// function isEqual(x, y) {
+//     if (x === y) {
+//       console.log('They are equal in type and value');
+//     } else if (x == y) {
+//       console.log('They are equal in value');
+//     } else {
+//       console.log('They are not equal');
+//     }
+//     return;
+//   }
+
+  equal(10, 10);
+  equal(10, "10");
